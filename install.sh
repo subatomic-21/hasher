fd482d2e81fc0cf3523ad2215b05f9a613787672
pat <kinggrick23@gmail.com>
2020-05-28 22:55:30 -0400

    bingo bongo

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-git install .
+pip install .
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-	name = 'hasher'
-	version = '0.1.0'
-	packages = ['pycli']
+	name = 'hasher',
+	version = '0.1.0',
+	packages = ['hasher'],
 	entry_points = {
 		'console_scripts': [
 			'hasher = hasher.__main__:main'
