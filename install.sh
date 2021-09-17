9ece963f0ba3d5358617b9cfce2d51c19022376b
Raiden <raiden@pop-os.localdomain>
2021-09-17 13:18:23 -0400

    add getpass dependency

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
+sudo apt install python3-pip
+
 pip install hashlib
 pip install colorama
 pip install click
 pip install pyfiglet
-
+pip install getpass
 pip install .
+ 
+
+pip3 install hashlib
+pip3 install colorama
+pip3 install click
+pip3 install pyfiglet
+
+pip3 install .
